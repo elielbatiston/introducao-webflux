e8c9e68cd437120eda32194eccc437999ec2ce1b
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-13 07:53:24 -0300

    created project

---
+++ src/main/java/com/schoolofnet/introducaowebflux/IntroducaoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.introducaowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoWebfluxApplication.class, args);
+	}
+
+}
